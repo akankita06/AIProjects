cb1ce3581406bfc67058564e5306f6f97e861a69
Ankita Khandelwal <akankita06@gmail.com>
2025-09-10 23:21:31 -0700

    Add on the wikipedia tool

--- 02 - Agent with tools/main.py
+++ 02 - Agent with tools/main.py
@@ -1,11 +1,11 @@
+from re import search
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_tool_calling_agent, AgentExecutor
-from langchain.tools import DuckDuckGoSearchRun
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
-from tools import search_tool
+from tools import search_tool, wikipedia_tool
 import os
 
 load_dotenv()
@@ -16,6 +16,7 @@ class SearchResponse(BaseModel):
     search_query: str = Field(description="The search query to be used to search the internet")
     best_result: str = Field(description="The best search result to be used to answer the user query")
     source: str = Field(description="The source of the best search result")
+    tools_used: list[str] = Field(description="The tools used to answer the user query from the tools provided")
 
 parser = PydanticOutputParser(pydantic_object=SearchResponse)
 
@@ -24,8 +25,9 @@ prompt = ChatPromptTemplate.from_messages(
         ("system", 
         """
         You are a search assistant that will help search the most updated information on the internet. 
-        Answer the user query and use necessary tools.
-        Wrap the output in this format - \n{format_instructions} and provide no other text
+        Answer the user query and use necessary and appropriatetools depending on the user query.
+        Wrap the output in this format - \n{format_instructions} and provide no other text,
+        You are only allowed to use the tools provided to you. If you don't find the relevant tool, say you don't know.
         """),
         ("placeholder", "{chat_history}"),
         ("human", "{query}"),
@@ -33,14 +35,15 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
-tools = [search_tool]
+tools = [search_tool, wikipedia_tool]
 agent= create_tool_calling_agent(
     llm=llm, prompt=prompt, tools=tools
 )
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-response = agent_executor.invoke({"query": "What is the latest news regarding H1b visa?"})
+# response = agent_executor.invoke({"query": "What is the latest news regarding H1b visa?"})
+response = agent_executor.invoke({"query": "What is capital of India?"})
 
 try:
     structured_response = parser.parse(response["output"])
--- 02 - Agent with tools/tools.py
+++ 02 - Agent with tools/tools.py
@@ -1,9 +1,17 @@
-from langchain_community.tools import DuckDuckGoSearchRun
-from langchain.tools import Tool
+from langchain_community.tools import DuckDuckGoSearchRun, WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_core.tools import Tool
 
 search = DuckDuckGoSearchRun()
 search_tool = Tool(
     name="search",
     func=search.run,
-    description="Search the internet for the best and latestinformation"
+    description="Search the internet for the best and latest information"
+)
+
+wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+wikipedia_tool = Tool(
+    name="wikipedia",
+    func=wikipedia.run,
+    description="Search the wikipedia for general information on a topic"
 )
